8f47ac3345817f74e6037aba54b8c52aabadd11b
Thomas Gray <thomas.gray@tgray.fios-router.home>
2020-01-08 21:45:59 -0500

    Add index and show actions for GuessedWords

--- backend/app/controllers/guessed_words_controller.rb
+++ backend/app/controllers/guessed_words_controller.rb
@@ -1,2 +1,15 @@
 class GuessedWordsController < ApplicationController
+
+  def index
+    words = GuessedWord.all
+    render json: words
+  end
+
+  def show
+    word = GuessedWord.find(params[:id])
+    render json: word
+  end
+
+  
+
 end
